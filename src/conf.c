f8b8eda5ca8d92c421f708d451d27a77368925fa
ewinds <dutboxer@gmail.com>
2014-07-07 10:40:25 +0800

    replace firewall ruleset with trusted host list

--- src/conf.c
+++ src/conf.c
@@ -711,7 +711,8 @@ config_read(const char *filename)
 							&linenum);
 					break;
 				case oFirewallRuleSet:
-					parse_firewall_ruleset(p1, fd, filename, &linenum);
+					// parse_firewall_ruleset(p1, fd, filename, &linenum);
+					parse_trusted_host_list(p1);
 					break;
 				case oTrustedMACList:
 					parse_trusted_mac_list(p1);
